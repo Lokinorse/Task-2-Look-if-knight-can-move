1b7d0077425601e5772433296b042d9850decea3
Ratibor <demonictype@gmail.com>
2019-06-03 11:25:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-/* const lines = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-const board = [];
-function createBoard() {
-  //Создаём одномерный массив со всеми клетками на доске
-  for (l = 0; l < lines.length; l++) {
-    for (i = 1; i < 9; i++) {
-      board.push(lines[l] + [i]);
-    }
-  }
-console.log(board)
-}
-
-createBoard(); */
-
 var app = new Vue({
   el: '#app',
   data: function () {
+    /* Создаём двумерный массив, который будет нашей виртуальной доской и возвращаем объект из этой в функции в data */
     const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     const rows = [1, 2, 3, 4, 5, 6, 7, 8];
     let board = columns.map(function (xValue, xIndex) {
       return rows.map(function (yValue, yIndex) {
         return {
-          isBlack: (yValue + xIndex) % 2 == true,
           position: { x: xIndex, y: yIndex },
           label: xValue + yValue,
 
@@ -31,8 +17,6 @@ var app = new Vue({
     return {
       input: '',
       board: board,
-      isWhite: true,
-      isBlack: false,
       knight: { x: 3, y: 3 },
       result: '',
     }
@@ -47,9 +31,11 @@ var app = new Vue({
       const userInput = this.input
       app.board.forEach(function (el) {
         el.forEach(function (el2) {
+          /* if проверяет совпадает ли введённые юзером данные с любым из лэйблов доски. Если так, то начинаем вычисления */
           if (userInput === el2.label) {
             app.board.forEach(function (el3) {
               el3.forEach(function (el4) {
+               /*  здесь мы проверяем добавляем в result лейблы клеток, куда может сходить конь */
                 if (el4.position.x === el2.position.x + 1 && el4.position.y === el2.position.y + 2) {
                   app.result = app.result + ' ' + el4.label;
                 }
